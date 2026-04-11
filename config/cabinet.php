f0c3b5c677721130537733e1164a417c23743e75
Roman Staroseltsev <r.staroseltsev@gmail.com>
2026-04-11 16:34:46 +0300

    refactor: introduce config-based menu item visibility

--- config/cabinet.php
+++ config/cabinet.php
@@ -4,4 +4,10 @@ return [
 
     'default_route' => env('CABINET_DEFAULT_ROUTE', '/cabinet'), // 1st route after login
 
+    'menu' => [
+        'items_hidden' => [
+//            'home',
+        ]
+    ]
+
 ];
